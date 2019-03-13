49eabb5c75febe63d921378c135e98f65f774117
LiYang07 <neowoolee@126.com>
2019-03-13 20:41:04 +0800

    modified:   leetcode.sln 	new file:   leetcode/19 Remove Nth Node From End of List/19 Remove Nth Node From End of List.vcxproj 	new file:   leetcode/19 Remove Nth Node From End of List/19 Remove Nth Node From End of List.vcxproj.filters 	new file:   leetcode/19 Remove Nth Node From End of List/Remove Nth Node From End of List.cpp

---
+++ leetcode/19 Remove Nth Node From End of List/Remove Nth Node From End of List.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+  };
+
+  class Solution {
+  public:
+	  ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+	  }
+  };
